eb59a6cf287b5dd937e91449dfaeeb21b36f0d8c
adarsh <adarsh.2104@gmail.com>
2021-09-26 02:38:11 +0530

    Removed test client app

--- request_client/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import SearchView,HomeView
-
-urlpatterns = [
-    path('search/<str:query>', SearchView.as_view(), name='search_views'),
-    path('suggest/', SearchView.as_view(), name='search_views'),
-    path('',HomeView.as_view(),name='home_view')
-]
