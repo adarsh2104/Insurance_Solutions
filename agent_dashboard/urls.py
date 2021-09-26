dc09de97295727a9d3da597fbff236e063912174
adarsh <adarsh.2104@gmail.com>
2021-09-26 15:11:15 +0530

    Updated route path name attribute for monthly data lookup view

--- agent_dashboard/urls.py
+++ agent_dashboard/urls.py
@@ -4,7 +4,7 @@ from agent_dashboard.views import SearchPolicyView, PolicyEditView, MonthlyDataB
 
 urlpatterns = [
     path('policy_edit/', PolicyEditView.as_view(), name='policy_edit_view'),
-    path('regions/', MonthlyDataByRegion.as_view(), name='monthly_data_lookup'),
+    path('regions/', MonthlyDataByRegion.as_view(), name='monthly_data_lookup_view'),
     path('<str:query_type>/<int:request_id>',SearchPolicyView.as_view(), name='policy_search_view')
 
 ]
