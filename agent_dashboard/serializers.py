518cc6c2af47750b6fa997b2f88ad0247c09b3ac
adarsh <adarsh.2104@gmail.com>
2021-09-26 15:10:18 +0530

    Added comments on model serilizer field for policy model

--- agent_dashboard/serializers.py
+++ agent_dashboard/serializers.py
@@ -2,10 +2,14 @@
 from .models import Policy,Customer
 from rest_framework import serializers
 from django.conf import settings
-# from .validators.field_validator import FieldValidator
 
 
 class PolicyDetailSerializer(serializers.ModelSerializer):
+    '''
+    Policy Model Serializer for serializing/deserializing Policy objects
+    purchase_date : Date Field formatted to "%d-%m-%Y"
+    '''
+    
     purchase_date = serializers.DateField(format="%d-%m-%Y", required=False, read_only=True)
     
     class Meta:
